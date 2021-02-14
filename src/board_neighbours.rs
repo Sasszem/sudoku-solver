3860e2b4fac43c0b540629ff1e352d5c6333ad77
Sasszem <barath.laszlo.szolnok@gmail.com>
2021-02-14 23:19:16 +0100

    refactor board_neighbours

--- src/board_neighbours.rs
+++ src/board_neighbours.rs
@@ -4,54 +4,23 @@ pub fn to_index(row: u8, column: u8) -> usize {
     return usize::from(9*row + column);
 }
 
-struct Neighbours {
-    row: u8,
-    column: u8,
-    current: u8 
+pub fn row_indexes(row: u8) -> impl Iterator<Item=usize> {
+    return (0..9).map(move |x| to_index(row, x));
 }
 
-impl Neighbours {
-    pub fn new(row: u8, column: u8) -> Neighbours {
-        Neighbours{
-            row,
-            column,
-            current: 0
-        }
-    }
+pub fn column_indexes(col: u8) -> impl Iterator<Item=usize> {
+    return (0..9).map(move |x| to_index(x, col));
 }
 
-impl Iterator for Neighbours {
-    type Item = usize;
-
-    fn next(&mut self) ->Option<Self::Item> {
-        if self.current==81 {
-            self.current = self.current + 1;
-            None
-        } else {
-            let m = self.current / 9;
-            let n = self.current % 9;
-            self.current =  self.current + 1;
-            match m {
-                0 => {
-                    Some(to_index(self.row, n))
-                }
-                1 => {
-                    Some(to_index(n, self.column))
-                }
-                2 => {
-                    let base_row = (self.row/3) * 3;
-                    let base_column = (self.column/3) * 3;
-                    let new_row = base_row + n/3;
-                    let new_column = base_column + n%3;
-                    Some(to_index(new_row, new_column))
-                }
-                _ => None
-            }
-        }
-    }
+pub fn square_indexes(sq_row: u8, sq_column: u8) -> impl Iterator<Item=usize> {
+    return (0..9).map(move |x| to_index(3*sq_row + x/3, 3*sq_column + x%3));
 }
 
-pub fn neighbours(row: u8, column: u8) -> impl Iterator<Item = usize>{
+pub fn neighbours(row: u8, column: u8) -> impl Iterator<Item = usize> {
     let idx = to_index(row, column);
-    return Neighbours::new(row, column).filter(move |&x| x!= idx);
+    let iter_row = row_indexes(row);
+    let iter_col = column_indexes(column);
+    let iter_sq = square_indexes(row/3, column/3);
+    let iter_ch = iter_row.chain(iter_col).chain(iter_sq);
+    return iter_ch.filter(move |&x| x!= idx);
 }
